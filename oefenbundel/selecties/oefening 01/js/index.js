9d1bea8c0f6324f9f0a24acbb443a1562582cd93
Emeline Bourgois <emeline.bourgois@mbp-van-emeline.home>
2023-09-12 18:38:20 +0200

    first commit

--- oefenbundel/selecties/oefening 01/js/index.js
+++ oefenbundel/selecties/oefening 01/js/index.js
@@ -1,8 +1,7 @@
-document.getElementById(submit).onclick = function()
-let leeftijd = document.getElementById(leeftijd);
+let leeftijd = prompt("wat is jouw leeftijd?(cijfernotatie)");
 
 if(leeftijd >= 18){
-    document.write("je bent ", leeftijd," oud. Je mag deelnemen aan de spelen van de Nationale Lotterij!");
-} else{ document.write("om deel te nemen aan de spelen van de Nationale lotterij moet je minstens 18 jaar oud zijn");
+    document.write("je bent ", leeftijd," oud. Je mag deelnemen aan de spelen van de Nationale Lotterij!")
+} else{ document.write("om deel te nemen aan de spelen van de Nationale lotterij moet je minstens 18 jaar oud zijn")
 
 }
